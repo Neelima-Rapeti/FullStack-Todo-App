e59a788b01175d8734da50dfdb8778981b8e85cc
cele <cele@cele.com>
2023-01-25 17:25:57 +0100

    add frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const data = async () => {
+      try {
+        const response = await axios.get("http://localhost:4040/todos");
+        setTodos(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    data();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>ToDoList🤓</h1>
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <p>{todo.value}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
